fea594bcf2d93300f87f43c0d4c47590ab8c5228
Onur Mete Bezgin <onur1905mete@gmail.com>
2024-11-20 15:33:15 +0300

    React-Quiz-Game

---
+++ quiz-game/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/header";
+import Quiz from "./components/Quiz";
+
+function App() {
+    return(
+        <>
+        <Header/>
+        <main>
+         <Quiz/>
+        </main>
+        </>
+    )
+}
+
+export default App;
---
+++ quiz-game/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import  logoİmg from "../assets/quiz-logo.png"
+
+export default function Header(){
+    return(
+        <header>
+        <img src={logoİmg}/>
+        <h1>ReactQuiz</h1>
+        </header>
+    )
+}
